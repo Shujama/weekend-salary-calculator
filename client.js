7b72b95a12847efe7f2d33fd9f4a0b7234bbc4ef
Shucayb Jama <shazam9963@gmail.com>
2025-01-15 17:59:15 -0600

    added delete button to each new row

--- client.js
+++ client.js
@@ -1,12 +1,15 @@
 function submitButton() {
     console.log("Submit button clicked!");
+     
+    //   appending to the field inputs  and ids in html
      let fNameInput = document.querySelector('#firstid').value;
      let lNameInput = document.querySelector('#lastid').value;
      let idInput = document.querySelector('#idid').value;
      let titleInput = document.querySelector('#titleid').value;
      let salaryInput = document.querySelector('#salaryid').value;
      let tableAppend = document.querySelector('#tableid');
-    
+
+    //   adding a row with the new information
     tableAppend.innerHTML += `
       <tr> 
         <td>${fNameInput}</td>
@@ -14,6 +17,13 @@ function submitButton() {
         <td>${idInput}</td>
         <td>${titleInput}</td>
         <td>${salaryInput}</td>
+        <td><button onclick = "">Delete</button></td>
      </tr>
    `
+    //    clearing the field inputs after submitting
+   fNameInput = '';
+   lNameInput = '';
+   idInput = '';
+   titleInput = '';
+   salaryInput = '';
    };
