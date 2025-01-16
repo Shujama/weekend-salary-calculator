7adf0320a4130b2bf13487f9bc2ed7f3ccd1a2e1
Shucayb Jama <shazam9963@gmail.com>
2025-01-15 19:06:09 -0600

    formatted salary inputs to money

--- client.js
+++ client.js
@@ -1,3 +1,12 @@
+
+totalMonthly = 0;
+let monthlyInput = document.querySelector('#monthlyid');
+//  Formatting salary input to currency
+let usDollar = new Intl.NumberFormat('en-US',{
+    style: 'currency',
+    currency: 'USD',
+});
+
 function submitButton() {
     console.log("Submit button clicked!");
      
@@ -8,7 +17,8 @@ function submitButton() {
      let titleInput = document.querySelector('#titleid').value;
      let salaryInput = document.querySelector('#salaryid').value;
      let tableAppend = document.querySelector('#tableid');
-
+     
+     let formattedSal = usDollar.format(salaryInput);
     //   adding a row with the new information
     tableAppend.innerHTML += `
       <tr> 
@@ -16,22 +26,37 @@ function submitButton() {
         <td>${lNameInput}</td>
         <td>${idInput}</td>
         <td>${titleInput}</td>
-        <td>${salaryInput}</td>
+        <td>${formattedSal}</td>
         <td><button onclick = "deleteButton(event)">Delete</button></td>
      </tr>
    `
+    //  calculating monthly salary
+    let msalary = salaryInput/12;
+      totalMonthly += msalary;
+
+    //   calling function to add salary to total monthly salary
+    addMonthlySalary ()
+
     //    clearing the field inputs after submitting
     document.querySelector('#firstid').value = '';
     document.querySelector('#lastid').value = '';
     document.querySelector('#idid').value = '';
     document.querySelector('#titleid').value = '';
     document.querySelector('#salaryid').value = '';
-   };
 
-   function deleteButton(event) {
+    };
+
+
+// function adds the monthly total salary
+function addMonthlySalary (){
+    formMonSal = usDollar.format(totalMonthly)
+ monthlyInput.innerHTML =``;
+ monthlyInput.innerHTML += `Total monthly: ${formMonSal}`
+};
+
+function deleteButton(event) {
     let deleteRow = event.target.parentNode.parentNode; //gets the row
     console.log(deleteRow);  // just to check it is correct
     
     deleteRow.remove(); //removes row
-    
-}
+};
